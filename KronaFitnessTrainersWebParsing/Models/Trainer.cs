586ce33055669f9c4f340974c34d9c2a48da4a3d
Student <Student@DESKTOP-JVBDPB7>
2026-05-11 20:58:41 +0300

    Добавьте файлы проекта.

---
+++ KronaFitnessTrainersWebParsing/Models/Trainer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KronaFitnessTrainersWebParsing.Models
+{
+    public class Trainer
+    {
+        public string Name { get; set; }
+
+        public string InfoHref { get; set; }
+    }
+}
---
+++ KronaFitnessTrainersWebParsing/Program.cs
@@ -0,0 +1,80 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Xml.Linq; // Работаем с LINQ to XML
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+using KronaFitnessTrainersWebParsing.Models;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        XDocument document = GetPageAsXDocument("https://www.kronafitness.ru/uslugi/");
+        document.Save("trainers_doc.xml");
+
+        var trainers = new List<Trainer>();
+
+        List<string> trainerNames = document.Descendants().
+            Where(e => e.Attribute("class")?.Value == "cl-ib-title")
+            .Select(e => e.Value).ToList();
+        Console.WriteLine($"Загружено тренеров: {trainerNames.Count}");
+
+        foreach (string name in trainerNames)
+        {
+            trainers.Add(new Trainer
+            {
+                Name = name,
+            });
+        }
+        List<string> trainerHrefs = document.Descendants()
+            .Where(e =>
+            {
+                bool con1 = e.Attribute("class")?.Value == "et_pb_text_inner";
+                bool con2 = e.Elements().Any(e => e.Name.LocalName == "a");
+                return con1 && con2;
+            })
+            .Select(e =>
+            {
+                var href = e.Elements().FirstOrDefault().Attribute("href").Value;
+                return href;
+            }).ToList();
+        Console.WriteLine($"Загружено ссылок: {trainerHrefs.Count}");
+
+        for (int i = 0; i < trainers.Count; i++)
+        {
+            trainers[i].InfoHref = trainerHrefs[i];
+        }
+
+        PrintTrainers(trainers);
+    }
+    public static XDocument GetPageAsXDocument(string url)
+    {
+        var options = new ChromeOptions();
+        options.AddArgument("--headless");
+        options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36");
+
+        using (IWebDriver driver = new ChromeDriver(options))
+        {
+            driver.Navigate().GoToUrl(url);
+            Thread.Sleep(3000);
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(driver.PageSource);
+            htmlDoc.OptionOutputAsXml = true;
+            Console.WriteLine(string.Join("; ", htmlDoc));
+            // Используем XDocument.Parse вместо XmlDocument.LoadXml
+            return XDocument.Parse(htmlDoc.DocumentNode.OuterHtml);
+        }
+    }
+
+    public static void PrintTrainers(List<Trainer> trainers)
+    {
+        foreach (var trainer in trainers)
+        {
+            Console.WriteLine($"{trainer.Name, 25}\t|\t{trainer.InfoHref}");
+        }
+    }
+}
