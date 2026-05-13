3b979dfbfceddfc3c44460a4fc4f5410268aa984
Student <Student@DESKTOP-JVBDPB7>
2026-05-13 18:53:46 +0300

    Добавил новые поля для полноценного парсинга тренеров

--- KronaFitnessTrainersWebParsing/Models/Trainer.cs
+++ KronaFitnessTrainersWebParsing/Models/Trainer.cs
@@ -9,5 +9,21 @@ namespace KronaFitnessTrainersWebParsing.Models
         public string Name { get; set; }
 
         public string InfoHref { get; set; }
+
+        public string Education {  get; set; }
+
+        public List<string> Specializations { get; set; }
+
+        public List<string> AdditionalEducations { get; set; }
+
+        public int TrainingExperienceYears { get; set; }
+
+        public int FitnessExperience {  get; set; }
+
+        public bool IsCMS {  get; set; }
+
+        public bool IsMS { get; set; }
+
+        public string LinkToWorkout { get; set; }
     }
 }
